87b9750837ce41bc0d560c879e6afa57e3418b51
Audrey Castillo <valax321@gmail.com>
2025-12-13 11:22:41 +1100

    Fix nuget packaging

--- build-nuget-packages.ps1
+++ build-nuget-packages.ps1
@@ -1,4 +1,4 @@
 #!/usr/bin/env pwsh
 
-& dotnet pack $PSScriptRoot/src/Radish.Windowing -c Release --include-symbols -o $PSScriptRoot/publish
-& dotnet pack $PSScriptRoot/src/Radish.Windowing.SDL3 -c Release --include-symbols -o $PSScriptRoot/publish
+& dotnet pack $PSScriptRoot/src/Radish.Windowing -c Release -o $PSScriptRoot/publish
+& dotnet pack $PSScriptRoot/src/Radish.Windowing.SDL3 -c Release -o $PSScriptRoot/publish
