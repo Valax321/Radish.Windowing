b14afbcddfa08eb0f6a7fabd65fbdb54d0b6295c
Audrey Castillo <valax321@gmail.com>
2026-07-14 16:57:58 +1000

    Fix doc comment

--- src/Radish.Windowing.SDL3/EventHandlerDelegate.cs
+++ src/Radish.Windowing.SDL3/EventHandlerDelegate.cs
@@ -3,7 +3,7 @@ using SDL3;
 namespace Radish.Windowing.SDL3;
 
 /// <summary>
-/// Event invoked for every <see cref="SDL.Event"/> processed by <see cref="SDL.PollEvent"/>.
+/// Event invoked for every <see cref="SDL.Event"/> processed by <see cref="SDL.PollEvent(out SDL.Event)"/>.
 /// <param name="event">The event data structure.</param>
 /// </summary>
 public delegate void EventHandlerDelegate(in SDL.Event @event);
